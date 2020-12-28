18b24d3379f307d54393b7ffb47d766903f9ce98
Jackeagle <jackeagle102@gmail.com>
2020-12-28 18:45:15 +0530

    Settings: Integrate TiElements into Settings
    - Adapt to Android R Changes
    - Keep it below Connected Devices
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -160,6 +160,8 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.titanium.tielements.Elements;
+
 public class SettingsGateway {
 
     /**
@@ -307,7 +309,9 @@ public class SettingsGateway {
             GestureNavigationSettingsFragment.class.getName(),
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
-            MediaControlsSettings.class.getName()
+            MediaControlsSettings.class.getName(),
+            // TitaniumOS additions
+            Elements.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -347,5 +351,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            // TitaniumOS additions
+            Settings.TiElementsSettingsActivity.class.getName(),
     };
 }
