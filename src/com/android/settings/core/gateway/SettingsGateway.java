Havoc-OS/android_packages_apps_Settings

Settings: Allow to quickly open Running Services from QS panel [2/2]

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -36,6 +36,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppAndNotificationDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
 import com.android.settings.applications.appinfo.DrawOverlayDetails;
@@ -291,6 +292,7 @@ public class SettingsGateway {
             ConfigCenter.class.getName(),
             GesturesSettings.class.getName(),
             GamingMode.class.getName(),
+            RunningServices.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -331,5 +333,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.ConfigCenterActivity.class.getName(),
             Settings.GamingModeActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }

Titanium-OS/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -36,6 +36,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppAndNotificationDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
 import com.android.settings.applications.appinfo.DrawOverlayDetails;
@@ -311,7 +312,8 @@ public class SettingsGateway {
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
             // TitaniumOS additions
-            Elements.class.getName()
+            Elements.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -353,5 +355,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             // TitaniumOS additions
             Settings.TiElementsSettingsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }